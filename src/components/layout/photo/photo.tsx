b9735c8f47ce2c57b9a5ffc34863c3f09b7de77e
Dmitry <ananiev_dima.96@mail.ru>
2023-09-14 17:43:04 +0300

    create base system

---
+++ src/components/layout/photo/photo.tsx
@@ -0,0 +1,5 @@
+export const Photo = () =>{
+  return (
+    <img src="" alt="" className="photo" />
+  )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
+
+import { Footer, Header } from '@@/components/base/index'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head />
       <body>
+        <Header />
         <Main />
+        <Footer />
         <NextScript />
       </body>
     </Html>

b4d0890657146dabead5302d370c5343a4acd01b
Dmitry <ananiev_dima.96@mail.ru>
2023-09-19 18:01:53 +0300

    style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <main>
       <section className={s.start_text}>
-        <h1>Ананьев Дмитрий</h1>
-        <h2>Frontend разработчик</h2>
+        <div className={s.center_block}>
+          <h1 className={s.title}>Ананьев Дмитрий</h1>
+          <h2 className={s.pretitle}>Frontend разработчик</h2>
+        </div>
       </section>
-
       <Photo />
-
     </main>
   )
 }
